0eb3b7a4167a07b45276ff5d226c754a7b502f19
Akhdan Fadhilah <me@akhdan.dev>
2025-12-23 20:52:13 +0900

    Add ch. 9 code: readers-writer problem

---
+++ ch09_deadlocks-and-starvation/reader_writer/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var (
+	counter int = 0 // shared memory
+	lock        = NewRWLock()
+)
+
+func user(idx int) {
+	for {
+		lock.AcquireRead()
+
+		fmt.Printf("User %d reading '%d'\n", idx, counter)
+		time.Sleep(time.Duration(1+rand.Intn(2)) * time.Second)
+
+		lock.ReleaseRead()
+		fmt.Printf("User %d done reading\n", idx)
+		time.Sleep(500 * time.Millisecond) // simulate time between reads
+	}
+}
+
+func librarian() {
+	for {
+		lock.AcquireWrite()
+
+		fmt.Print("Librarian writing... ")
+		time.Sleep(time.Duration(1+rand.Intn(2)) * time.Second) // simulate writing time
+		counter++
+		fmt.Printf("New value: %d\n", counter)
+
+		lock.ReleaseWrite()
+	}
+}
+
+func main() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() { defer wg.Done(); user(0) }()
+	go func() { defer wg.Done(); user(1) }()
+	go func() { defer wg.Done(); librarian() }()
+	wg.Wait()
+}
---
+++ ch09_deadlocks-and-starvation/reader_writer/rwlock.go
@@ -0,0 +1,49 @@
+package main
+
+import "sync"
+
+// RWLock is a lock that allows multiple readers or one writer at a time.
+type RWLock struct {
+	readers   int
+	readLock  sync.Mutex
+	writeLock sync.Mutex
+}
+
+func NewRWLock() *RWLock { return &RWLock{} }
+
+// AcquireRead acquires the read lock for the current thread.
+// If there is a writer waiting for the lock, the method blocks until the writer releases the lock.
+func (l *RWLock) AcquireRead() {
+	l.readLock.Lock()
+	l.readers++
+	if l.readers == 1 {
+		l.writeLock.Lock()
+	}
+	l.readLock.Unlock()
+}
+
+// ReleaseRead releases the read lock held by the current thread.
+// If there are no more readers holding the lock, the method releases the write lock.
+func (l *RWLock) ReleaseRead() {
+	if l.readers < 1 {
+		panic("ReleaseRead called without a matching AcquireRead")
+	}
+
+	l.readLock.Lock()
+	l.readers--
+	if l.readers == 0 {
+		l.writeLock.Unlock()
+	}
+	l.readLock.Unlock()
+}
+
+// AcquireWrite acquires the write lock for the current thread.
+// If there is a reader or a writer holding the lock, the method blocks until the lock is released.
+func (l *RWLock) AcquireWrite() {
+	l.writeLock.Lock()
+}
+
+// ReleaseWrite releases the write lock held by the current thread.
+func (l *RWLock) ReleaseWrite() {
+	l.writeLock.Unlock()
+}
